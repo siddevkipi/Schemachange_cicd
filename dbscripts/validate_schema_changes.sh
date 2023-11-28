91eea10f336b015d7c94dc325ebf19f3c373265e
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-11-28 20:30:46 +0530

    Update validate_schema_changes.sh

--- dbscripts/validate_schema_changes.sh
+++ dbscripts/validate_schema_changes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for file in $(find . -type f -name '*.sql'); do
-    if ! [[ ("$file") =~ ^V\d+\.\d+\.\d+__[a-zA-Z0-9_]+\.sql$ ]]; then
+    if ! [[ (basename "$file") =~ ^V\d+\.\d+\.\d+__[a-zA-Z0-9_]+\.sql$ ]]; then
         echo "Error: Invalid naming convention in $file"
         exit 1
     fi
