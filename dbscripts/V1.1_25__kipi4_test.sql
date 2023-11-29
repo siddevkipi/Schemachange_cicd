fdf79694209ed32b34bd1ac731be6f5d195a27d8
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-11-29 20:25:15 +0530

    Update and rename V1.1_24__kipi4_test.sql to V1.1_25__kipi4_test.sql

--- dbscripts/V1.1_25__kipi4_test.sql
+++ dbscripts/V1.1_25__kipi4_test.sql
@@ -1,6 +1,6 @@
 use database DEMO_DB;
-create schema KIPI7;
-create table kipi_test_users7 if not exists(
+create schema KIPI9;
+create table kipi_test_users9 if not exists(
   name varchar (100),  -- variable string column
   preferences string, -- column used to store JSON type of data
   created_at timestamp
