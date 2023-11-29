23583056ee925636708b755bd639d78e7a758d7b
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-11-29 19:39:05 +0530

    Update validate_schema_changes.sh

--- dbscripts/validate_schema_changes.sh
+++ dbscripts/validate_schema_changes.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-for file in $(find . -type f -name '*.sql'  -path './dbscripts/*'); do
-    echo "$(basename "$file")"
-    if ! [[ $(basename "$file") == ^[vV]\d+\.\d+\.\d+__[a-zA-Z0-9_]+\.sql$ ]]; then
-        echo "Error: Invalid naming convention in $file"
-        exit 1
-    fi
+directory="dbscripts/"
+pattern="^[vV]\d+\.\d+\.\d+__[a-zA-Z0-9_]+\.sql$"
 
-    # Add additional checks as needed
+# Use find to get a list of files in the directory
+find "$directory" -type f | while read -r file; do
+    # Extract the file name from the full path
+    file_name=$(basename "$file")
+	echo "$file_name"
+
+    # Check if the file name matches the pattern
+    if [[ $file_name =~ $pattern ]]; then
+        echo "File '$file_name' matches the pattern."
+    else
+        echo "File '$file_name' does not match the pattern."
+    fi
 done
 
 
