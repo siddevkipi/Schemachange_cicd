90af5b2bf4aeed15891ebb5a8a4eccb88b150c46
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-11-30 14:36:51 +0530

    Update sid.py

--- dbscripts/sid.py
+++ dbscripts/sid.py
@@ -10,14 +10,18 @@ for entry in os.scandir(directory):
         if re.match(pattern, file_name):
             print(f"File '{file_name}' matches the pattern. Proceeding with schemachange.")
             print("Running schemachange")
+            os.chdir(directory)
             schemachange_command = (
-                f"schemachange -f {os.path.join(directory, file_name)} -a {os.environ['SF_ACCOUNT']} "
-                f"-u {os.environ['SF_USERNAME']} -r {os.path.abspath(directory)} "
+                f"schemachange -f {file_name} -a {os.environ['SF_ACCOUNT']} "
+                f"-u {os.environ['SF_USERNAME']} -r . "  # Use '.' to represent the current directory as root
                 f"-w {os.environ['SF_WAREHOUSE']} -d {os.environ['SF_DATABASE']} "
                 f"-c {os.environ['SF_DATABASE']}.SCHEMACHANGE.CHANGE_HISTORY --create-change-history-table"
             )
             subprocess.run(schemachange_command, shell=True, check=True)
             
+            # Change back to the original working directory
+            os.chdir('..')
+
         else:
             print(f"File '{file_name}' does not match the pattern. Skipping schemachange.")
             continue
