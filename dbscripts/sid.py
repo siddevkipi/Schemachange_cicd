115a2b19d2433bf28eff2b53c4f73d22b1f5bde3
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-11-30 12:59:12 +0530

    Update sid.py

--- dbscripts/sid.py
+++ dbscripts/sid.py
@@ -17,10 +17,10 @@ for entry in os.scandir(directory):
             print(f"File '{file_name}' matches the pattern. Proceeding with schemachange.")
 
             # Add schemachange logic here
-            echo "Step 1: Installing schemachange"
-            pip install schemachange
-            echo "Step 2: Running schemachange"
-            schemachange -f $GITHUB_WORKSPACE/dbscripts -a $SF_ACCOUNT -u $SF_USERNAME -r $SF_ROLE -w $SF_WAREHOUSE -d $SF_DATABASE -c $SF_DATABASE.SCHEMACHANGE.CHANGE_HISTORY --create-change-history-table
+          echo "Step 1: Installing schemachange"
+          pip install schemachange
+          echo "Step 2: Running schemachange"
+          schemachange -f $GITHUB_WORKSPACE/dbscripts -a $SF_ACCOUNT -u $SF_USERNAME -r $SF_ROLE -w $SF_WAREHOUSE -d $SF_DATABASE -c $SF_DATABASE.SCHEMACHANGE.CHANGE_HISTORY --create-change-history-table
             
         else:
             print(f"File '{file_name}' does not match the pattern. Skipping schemachange.")
