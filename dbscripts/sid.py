e0e7be1ff980bb18c1034ec3d12a4c59cf3cef25
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-11-30 14:40:06 +0530

    Update sid.py

--- dbscripts/sid.py
+++ dbscripts/sid.py
@@ -1,27 +1,31 @@
 import os
-import re
 import subprocess
+
 directory = "dbscripts/"
 pattern = r"^[vV]\d+\.\d+\.\d+__[a-zA-Z0-9_]+\.sql$"
-for entry in os.scandir(directory):
-    if entry.is_file():
-        file_name = entry.name
-        print(file_name)
-        if re.match(pattern, file_name):
-            print(f"File '{file_name}' matches the pattern. Proceeding with schemachange.")
-            print("Running schemachange")
-            os.chdir(directory)
-            schemachange_command = (
-                f"schemachange -f {file_name} -a {os.environ['SF_ACCOUNT']} "
-                f"-u {os.environ['SF_USERNAME']} -r . "  # Use '.' to represent the current directory as root
-                f"-w {os.environ['SF_WAREHOUSE']} -d {os.environ['SF_DATABASE']} "
-                f"-c {os.environ['SF_DATABASE']}.SCHEMACHANGE.CHANGE_HISTORY --create-change-history-table"
-            )
-            subprocess.run(schemachange_command, shell=True, check=True)
-            
-            # Change back to the original working directory
-            os.chdir('..')
 
-        else:
-            print(f"File '{file_name}' does not match the pattern. Skipping schemachange.")
-            continue
+for entry in os.scandir(directory):
+    if entry.is_file() and entry.name.endswith(".sql"):
+        file_name = entry.name
+        print(f"Processing file: {file_name}")
+
+        # Change the current working directory to the scripts directory
+        os.chdir(directory)
+
+        # Specify the file as the root folder in the command
+        schemachange_command = (
+            f"schemachange -f {file_name} -a {os.environ['SF_ACCOUNT']} "
+            f"-u {os.environ['SF_USERNAME']} -r . "  # Use '.' to represent the current directory as root
+            f"-w {os.environ['SF_WAREHOUSE']} -d {os.environ['SF_DATABASE']} "
+            f"-c {os.environ['SF_DATABASE']}.SCHEMACHANGE.CHANGE_HISTORY --create-change-history-table"
+        )
+
+        try:
+            subprocess.run(schemachange_command, shell=True, check=True)
+            print(f"Schemachange completed for file: {file_name}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error running schemachange for file '{file_name}'. Return code: {e.returncode}")
+            print(e)
+
+        # Change back to the original working directory
+        os.chdir('..')
