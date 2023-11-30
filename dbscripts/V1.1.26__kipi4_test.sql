90aedc922dd0121c391389cd56b8732c3eeb835c
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-11-30 13:59:59 +0530

    Update V1.1.26__kipi4_test.sql

--- dbscripts/V1.1.26__kipi4_test.sql
+++ dbscripts/V1.1.26__kipi4_test.sql
@@ -1,7 +1,2 @@
 use database DEMO_DB;
 create schema KIPI12;
-create table kipi_test_users12 if not exists(
-  name varchar (100),  -- variable string column
-  preferences string, -- column used to store JSON type of data
-  created_at timestamp
-);
