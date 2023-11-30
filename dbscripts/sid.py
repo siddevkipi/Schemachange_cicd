a5c29e183951a87aa164ab4c876cd7529f1b1962
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-11-30 13:28:29 +0530

    Update sid.py

--- dbscripts/sid.py
+++ dbscripts/sid.py
@@ -18,8 +18,8 @@ for entry in os.scandir(directory):
 
           # Add schemachange logic here
           
-          echo "Running schemachange"
-          schemachange -f $GITHUB_WORKSPACE/dbscripts -a $SF_ACCOUNT -u $SF_USERNAME -r $SF_ROLE -w $SF_WAREHOUSE -d $SF_DATABASE -c $SF_DATABASE.SCHEMACHANGE.CHANGE_HISTORY --create-change-history-table
+          subprocess.run('echo "Running schemachange"')
+          subprocess.run('schemachange -f $GITHUB_WORKSPACE/dbscripts -a $SF_ACCOUNT -u $SF_USERNAME -r $SF_ROLE -w $SF_WAREHOUSE -d $SF_DATABASE -c $SF_DATABASE.SCHEMACHANGE.CHANGE_HISTORY --create-change-history-table')
             
         else:
             print(f"File '{file_name}' does not match the pattern. Skipping schemachange.")
