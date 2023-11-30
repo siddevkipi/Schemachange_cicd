11ebe52b14982c04641d8fd80c0a81b60557aecf
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-11-30 13:32:09 +0530

    Update sid.py

--- dbscripts/sid.py
+++ dbscripts/sid.py
@@ -15,11 +15,8 @@ for entry in os.scandir(directory):
         # Check if the file name matches the pattern
         if re.match(pattern, file_name):
             print(f"File '{file_name}' matches the pattern. Proceeding with schemachange.")
-
-          # Add schemachange logic here
-          
-          subprocess.run('echo "Running schemachange"')
-          subprocess.run('schemachange -f $GITHUB_WORKSPACE/dbscripts -a $SF_ACCOUNT -u $SF_USERNAME -r $SF_ROLE -w $SF_WAREHOUSE -d $SF_DATABASE -c $SF_DATABASE.SCHEMACHANGE.CHANGE_HISTORY --create-change-history-table')
+            subprocess.run('echo "Running schemachange"')
+            subprocess.run('schemachange -f $GITHUB_WORKSPACE/dbscripts -a $SF_ACCOUNT -u $SF_USERNAME -r $SF_ROLE -w $SF_WAREHOUSE -d $SF_DATABASE -c $SF_DATABASE.SCHEMACHANGE.CHANGE_HISTORY --create-change-history-table')
             
         else:
             print(f"File '{file_name}' does not match the pattern. Skipping schemachange.")
