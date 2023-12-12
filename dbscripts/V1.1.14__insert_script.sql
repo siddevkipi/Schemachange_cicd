893d2ee50dd35343eee37f3fc43ffc1937bdb0d4
siddevkipi <151723928+siddevkipi@users.noreply.github.com>
2023-12-12 23:10:08 +0530

    Update and rename V1.1.31__insert_script.sql to V1.1.14__insert_script.sql

---
+++ dbscripts/V1.1.14__insert_script.sql
@@ -0,0 +1,1 @@
+insert into DEMO_DB.PUBLIC.FIDELITY_TEST_USERS values('rajesh','jindal','2020-01-01 12:23:23');
--- dbscripts/V1.1.31__insert_script.sql
+++
@@ -1,1 +0,0 @@
-insert into DEMO_DB.PUBLIC.FIDELITY_TEST_USERS values('Koyal','jindal','2020-01-01 12:23:23');
